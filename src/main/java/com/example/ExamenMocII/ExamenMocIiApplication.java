5340f21663e70359f9d6b49b542c14fb79d464be
Iván Iarunichev <ivani94@educastur.es>
2025-11-21 17:18:56 +0100

    Initial commit

---
+++ src/main/java/com/example/ExamenMocII/ExamenMocIiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMocII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMocIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMocIiApplication.class, args);
+	}
+
+}
