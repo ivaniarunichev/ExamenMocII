a7eac0700a01c1c31dd1b507e443002435c00565
Iván Iarunichev <ivani94@educastur.es>
2025-11-21 18:08:03 +0100

    Organizacion Proyecto

---
+++ src/main/java/com/example/ExamenMocII/controller/ProductoController.java
@@ -0,0 +1,32 @@
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @PostMapping(value = "/productos")
+    public Producto addProducto(@RequestBody Producto producto) {
+    }
+
+    @DeleteMapping(value = "/producto/{productoId}")
+    public void deleteProducto(@PathVariable Long productoId) {
+    }
+
+    @PutMapping(value = "/producto/{productoId}")
+    public Producto modificarProducto(@PathVariable Long productoId, @RequestBody Producto producto) {
+    }
+
+    @GetMapping(value = "/productos")
+    public List<Producto> getProductos(@RequestParam(defaultValue = "0.0") Float precio,
+                                     @RequestParam(defaultValue = "") String categoria) {
+        /*
+            - Si no se indica ni precio ni categoría -> obtener todos los productos.
+            - Si se indica el precio -> obtener los productos con ese precio.
+            - Si se indica la categoria -> obtener los productos con esa categoria.
+         */
+    }
+
+    @GetMapping(value = "/producto/{productoId}")
+    public Optional<Producto> getProducto(@PathVariable Long productoId) {
+    }
+
+}
