843dc573262df0c23c14160bf501b190f7426fb7
Iván Iarunichev <ivani94@educastur.es>
2025-11-21 18:52:28 +0100

    Arreglando error en ProductoController

--- src/main/java/com/example/ExamenMocII/controller/ProductoController.java
+++ src/main/java/com/example/ExamenMocII/controller/ProductoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 public class ProductoController {
-
+    //ProductoController solucionado
     @Autowired
     private ProductoService productoService;
 
